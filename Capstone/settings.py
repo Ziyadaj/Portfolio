2a9bb67b9713c3933667c8d0cd1a1a530b9bf32e
Ziyadaj <91215743+Ziyadaj@users.noreply.github.com>
2023-04-09 23:27:40 +0300

    Update settings.py

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -127,7 +127,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ziyadaj0@gmail.com'
-EMAIL_HOST_PASSWORD = 'qxiwdjszzgcxwrvz'
+
 EMAIL_USE_SSL = False
 
